e48d285bd6db6834c34fefa9ee8e1d352737087e
Richard Liu <richy.liu.2002@gmail.com>
2021-11-14 15:13:20 -0600

    remove mouse keys

--- keyboard/keymap.c
+++ keyboard/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "pointing_device.h"
 
-enum layers { BASE, NOM, QWE, ARR, NAV, MOU, FUN, NUM, SYM, PLVR };
+enum layers { BASE, QWE, ARR, NAV, FUN, NUM, SYM, PLVR };
 
 #define U_RDO SCMD(KC_Z)
 #define U_PST LCMD(KC_V)
@@ -17,16 +17,7 @@ enum planck_keycodes {
   HEX_SLX, // sends "\x"
   HEX_ZRX, // sends "0x"
   HEX_ZZ,  // sends "00"
-  MY_CAPS,
-  MS_RST,
-  MS_LEFT,
-  MS_RGHT,
-  MS_UP,
-  MS_DOWN,
-  MS_UPLF,
-  MS_UPRT,
-  MS_DWLF,
-  MS_DWRT
+  MY_CAPS
 };
 
 // use an unused keycode because of mod-tap keycode restrictions:
@@ -35,6 +26,8 @@ enum planck_keycodes {
 
 #define OS_ALT OSM(MOD_LALT)
 #define OS_SFT OSM(MOD_LSFT)
+#define OS_CTL OSM(MOD_LCTL)
+#define OS_GUI OSM(MOD_LGUI)
 #define CT LCTL_T
 #define GT LGUI_T
 
@@ -61,14 +54,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ortho_4x12(
   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    C(KC_B), C(KC_B), KC_J,    KC_L,    KC_U,    KC_Y,      KC_QUOT,
   KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    OS_ALT,  OS_ALT,  KC_M,    KC_N,    KC_E,    KC_I,      KC_O,
-  GT(KC_Z),CT(KC_X),KC_C,    KC_D,    KC_V,    MO(MOU), MO(MOU), KC_K,    KC_H,    KC_COMM, CT(KC_DOT),GT(KC_SLSH),
-  CMD_TAB, OS_ALT,  SFT_ESC, NAV_BSPC,NUM_TAB, OS_SFT,  OS_SFT,  SYM_ENT, KC_SPC,  S_ABSPC, XXXXXXX,   MO(FUN)
-),
-[NOM] = LAYOUT_ortho_4x12(
-  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    XXXXXXX, XXXXXXX, KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,
-  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    XXXXXXX, XXXXXXX, KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
-  KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    XXXXXXX, XXXXXXX, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
-  XXXXXXX, XXXXXXX, KC_ESC,  KC_BSPC, KC_TAB,  XXXXXXX, XXXXXXX, KC_ENT,  KC_SPC,  KC_DEL,  DF(BASE),XXXXXXX
+  GT(KC_Z),CT(KC_X),KC_C,    KC_D,    KC_V,    OS_CTL,  OS_CTL,  KC_K,    KC_H,    KC_COMM, CT(KC_DOT),GT(KC_SLSH),
+  CMD_TAB, OS_GUI,  SFT_ESC, NAV_BSPC,NUM_TAB, OS_SFT,  OS_SFT,  SYM_ENT, KC_SPC,  S_ABSPC, XXXXXXX,   MO(FUN)
 ),
 [QWE] = LAYOUT_ortho_4x12(
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX, XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
@@ -88,15 +75,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LGUI, KC_LCTL, KC_LALT, KC_LSFT, G(KC_GRV),XXXXXXX,XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,XXXXXXX, KC_ENT,  KC_SPC,  KC_LSFT, XXXXXXX, XXXXXXX
 ),
-[MOU] = LAYOUT_ortho_4x12(
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MS_UPLF, MS_UP,   MS_UPRT, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MS_LEFT, KC_BTN1, MS_RGHT, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MS_DWLF, MS_DOWN, MS_DWRT, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MS_RST,  XXXXXXX, XXXXXXX, XXXXXXX
-),
 [FUN] = LAYOUT_ortho_4x12(
   KC_F12,  KC_F7,   KC_F8,   KC_F9,   XXXXXXX, XXXXXXX, DM_REC2, DM_REC1, DM_RSTP, DF(QWE), PLOVER,  RESET,
-  KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_BRMU, XXXXXXX, DM_PLY2, DM_PLY1, SELC_EL, INSP_EL, DF(NOM), DF(ARR),
+  KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_BRMU, XXXXXXX, DM_PLY2, DM_PLY1, SELC_EL, INSP_EL, XXXXXXX, DF(ARR),
   KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_BRMD, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,
   XXXXXXX, XXXXXXX, KC_LSFT, KC_DEL,  KC_TAB,  XXXXXXX, XXXXXXX, SCRSAVE, SCRCLIP, XXXXXXX, XXXXXXX, XXXXXXX
 ),
@@ -143,77 +124,6 @@ uint16_t cmd_tab_hold_timer = 0;
 bool is_cmd_tab_before_first = false;
 uint16_t cmd_tab_before_first_timer = 0;
 #define CMD_TAB_BEFORE_FIRST_TIMEOUT 3000
-
-static const int16_t WINDOW_WIDTH = 2560;
-static const int16_t WINDOW_HEIGHT = 1440;
-// binary search boundary for custom mouse keys
-int16_t ms_bs_left = 0;
-int16_t ms_bs_right = WINDOW_WIDTH;
-int16_t ms_bs_up = 0;
-int16_t ms_bs_down = WINDOW_HEIGHT;
-
-// three piecewise linear functions are needed to convert pixel values to mouse
-// input values
-// https://www.desmos.com/calculator/mms2oq4qrf
-static const double scale = 1.00;
-static const double a1 = scale * 1;
-static const double b1 = scale * 2.1;
-static const double a2 = scale * 0.413;
-static const double b2 = scale * 15.5;
-static const double a3 = scale * 1.02;
-static const double b3 = scale * -66.9;
-static const double d1 = scale * 22.9;
-static const double d2 = scale * 136.6;
-static const double d3 = scale * 190;
-// max travel distance (pixels) corresponds to the max raw (mouse input) value
-static const int16_t MAX_TRAVEL_DISTANCE = d3;
-static const int8_t MAX_RAW_DISTANCE = 127;
-
-// convert screen pixels to mouse input values
-int8_t convert_coord(int16_t dist) {
-  if (dist == 0) return 0;
-  if (dist < 0) return -convert_coord(-dist);
-
-  if (dist < d1) return a1 * dist + b1;
-  if (dist < d2) return a2 * dist + b2;
-  if (dist < d3 || dist == MAX_TRAVEL_DISTANCE) return a3 * dist + b3;
-
-  // too high
-  return 0;
-}
-
-report_mouse_t currentReport;
-// send raw mouse movements
-void move_mouse_raw(int8_t x, int8_t y) {
-  currentReport = pointing_device_get_report();
-  currentReport.x = x;
-  currentReport.y = y;
-  host_mouse_send(&currentReport);
-}
-
-// move the mouse given screen pixels
-// will first move in the horizontal direction, then the vertical direction
-// automatically converts pixels to mouse input units
-void move_mouse_delta(int16_t x, int16_t y) {
-  int8_t x_sign = x < 0 ? -1 : 1;
-  int8_t y_sign = y < 0 ? -1 : 1;
-
-  for (int16_t cur_x = abs(x); cur_x > 0; cur_x -= MAX_TRAVEL_DISTANCE) {
-    if (cur_x < MAX_TRAVEL_DISTANCE) {
-      move_mouse_raw(x_sign * convert_coord(cur_x), 0);
-    } else {
-      move_mouse_raw(x_sign * MAX_RAW_DISTANCE, 0);
-    }
-  }
-
-  for (int16_t cur_y = abs(y); cur_y > 0; cur_y -= MAX_TRAVEL_DISTANCE) {
-    if (cur_y < MAX_TRAVEL_DISTANCE) {
-      move_mouse_raw(0, y_sign * convert_coord(cur_y));
-    } else {
-      move_mouse_raw(0, y_sign * MAX_RAW_DISTANCE);
-    }
-  }
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // check for mod-tap alt-backspace separately to optimize the switch statement
@@ -283,99 +193,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case MY_CAPS:
       if (record->event.pressed) {
         tap_code_delay(KC_CAPS, 200);
-      }
-      return false;
-    case MS_RST:
-      if (record->event.pressed) {
-        // move mouse to upper-left corner to calibrate position
-        for (uint8_t i = 0; i < 20; i++) {
-          move_mouse_raw(-MAX_RAW_DISTANCE, -MAX_RAW_DISTANCE);
-        }
-        // initialize mouse binary search
-        ms_bs_left = 0;
-        ms_bs_right = WINDOW_WIDTH;
-        ms_bs_up = 0;
-        ms_bs_down = WINDOW_HEIGHT;
-        // start binary search at the middle
-        int16_t x_middle = (ms_bs_left + ms_bs_right) / 2;
-        int16_t y_middle = (ms_bs_up + ms_bs_down) / 2;
-        move_mouse_delta(x_middle, y_middle);
-      }
-      return false;
-    case MS_LEFT:
-      if (record->event.pressed) {
-        int16_t old_middle = (ms_bs_left + ms_bs_right) / 2;
-        int16_t new_middle = (ms_bs_left + old_middle) / 2;
-        move_mouse_delta(new_middle - old_middle, 0);
-        ms_bs_right = old_middle;
-      }
-      return false;
-    case MS_RGHT:
-      if (record->event.pressed) {
-        int16_t old_middle = (ms_bs_left + ms_bs_right) / 2;
-        int16_t new_middle = (old_middle + ms_bs_right) / 2;
-        move_mouse_delta(new_middle - old_middle, 0);
-        ms_bs_left = old_middle;
-      }
-      return false;
-    case MS_UP:
-      if (record->event.pressed) {
-        int16_t old_middle = (ms_bs_up + ms_bs_down) / 2;
-        int16_t new_middle = (ms_bs_up + old_middle) / 2;
-        move_mouse_delta(0, new_middle - old_middle);
-        ms_bs_down = old_middle;
-      }
-      return false;
-    case MS_DOWN:
-      if (record->event.pressed) {
-        int16_t old_middle = (ms_bs_up + ms_bs_down) / 2;
-        int16_t new_middle = (old_middle + ms_bs_down) / 2;
-        move_mouse_delta(0, new_middle - old_middle);
-        ms_bs_up = old_middle;
-      }
-      return false;
-    case MS_UPLF:
-      if (record->event.pressed) {
-        int16_t old_middle_y = (ms_bs_up + ms_bs_down) / 2;
-        int16_t new_middle_y = (ms_bs_up + old_middle_y) / 2;
-        int16_t old_middle_x = (ms_bs_left + ms_bs_right) / 2;
-        int16_t new_middle_x = (ms_bs_left + old_middle_x) / 2;
-        move_mouse_delta(new_middle_x - old_middle_x, new_middle_y - old_middle_y);
-        ms_bs_right = old_middle_x;
-        ms_bs_down = old_middle_y;
-      }
-      return false;
-    case MS_UPRT:
-      if (record->event.pressed) {
-        int16_t old_middle_y = (ms_bs_up + ms_bs_down) / 2;
-        int16_t new_middle_y = (ms_bs_up + old_middle_y) / 2;
-        int16_t old_middle_x = (ms_bs_left + ms_bs_right) / 2;
-        int16_t new_middle_x = (old_middle_x + ms_bs_right) / 2;
-        move_mouse_delta(new_middle_x - old_middle_x, new_middle_y - old_middle_y);
-        ms_bs_left = old_middle_x;
-        ms_bs_down = old_middle_y;
-      }
-      return false;
-    case MS_DWLF:
-      if (record->event.pressed) {
-        int16_t old_middle_y = (ms_bs_up + ms_bs_down) / 2;
-        int16_t new_middle_y = (old_middle_y + ms_bs_down) / 2;
-        int16_t old_middle_x = (ms_bs_left + ms_bs_right) / 2;
-        int16_t new_middle_x = (ms_bs_left + old_middle_x) / 2;
-        move_mouse_delta(new_middle_x - old_middle_x, new_middle_y - old_middle_y);
-        ms_bs_right = old_middle_x;
-        ms_bs_up = old_middle_y;
-      }
-      return false;
-    case MS_DWRT:
-      if (record->event.pressed) {
-        int16_t old_middle_y = (ms_bs_up + ms_bs_down) / 2;
-        int16_t new_middle_y = (old_middle_y + ms_bs_down) / 2;
-        int16_t old_middle_x = (ms_bs_left + ms_bs_right) / 2;
-        int16_t new_middle_x = (old_middle_x + ms_bs_right) / 2;
-        move_mouse_delta(new_middle_x - old_middle_x, new_middle_y - old_middle_y);
-        ms_bs_left = old_middle_x;
-        ms_bs_up = old_middle_y;
       }
       return false;
   }
