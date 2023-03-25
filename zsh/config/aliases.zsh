ac7b6e9bc0b3a8c3e73441824e9af3027ed11baa
Richard Liu <richy.liu.2002@gmail.com>
2023-03-25 15:08:54 -0500

    zsh: add basic bashrc

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -1,7 +1,6 @@
-alias vim=nvim
-alias vimb="nvim -u ~/config/vim/basic.vim"
-alias vimbs="nvim -u ~/config/vim/basic.vim +syntax\ off"
-alias vimsec="nvim -c 'set shada=' -c 'set noswapfile noundofile nobackup nowritebackup'"
+alias vimb="vim -u ~/config/vim/basic.vim"
+alias vimbs="vim -u ~/config/vim/basic.vim +syntax\ off"
+alias vimsec="vim -c 'set shada=' -c 'set noswapfile noundofile nobackup nowritebackup'"
 alias ag="ag -f --color-line-number 1\;35"
 alias agq="ag -fQ --color-line-number 1\;35"
 alias agl="ag -f --nonumbers --pager less"
@@ -13,6 +12,8 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 
+alias l="ls -la"
+alias ll="ls -lh"
 alias lltr="ll -tr"
 alias llsr="ll -Sr"
 
